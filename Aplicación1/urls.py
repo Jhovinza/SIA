5320102b90ee96b7a316a96769879060a9ad3bee
Jhovinza <jhovinza.moreno@alumnos.ucn.cl>
2021-06-21 23:37:28 -0400

    Sexto Entregable

--- Aplicación1/urls.py
+++ Aplicación1/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
-from Aplicación1.views import Aplicación1, listaClientes, agregarClientes, listaPlanes, agregarPlanes, archivoReportes
+from Aplicación1.views import Aplicación1, listaClientes, agregarClientes, listaPlanes, agregarPlanes, archivoReportes, \
+    modificarClientes, eliminarClientes, modificarPlanes, eliminarPlanes, calendario
 
 urlpatterns = [
     path("", Aplicación1),
@@ -11,5 +12,17 @@ urlpatterns = [
     path('agregarPlanes/', agregarPlanes, name='agregarPlanes'),
 
     path('reportes/', archivoReportes, name='reportes'),
+
+    path('modificarClientes/<id_cliente>', modificarClientes, name='modificarClientes'),
+    path('eliminarClientes/<id_cliente>', eliminarClientes, name='eliminarClientes'),
+
+    path('modificarPlanes/<id_plan>', modificarPlanes, name='modificarPlanes'),
+    path('eliminarPlanes/<id_plan>', eliminarPlanes, name='eliminarPlanes'),
+
+#path('<int:year>/<str:month>', calendario, name='calendario')
+#path('calendario/<year>/<month>', calendario, name='calendario')
+#path('calendario', calendario, name='calendario')
+
+
 ]
 
--- Aplicación1/views.py
+++ Aplicación1/views.py
@@ -4,6 +4,9 @@ from django.shortcuts import render, redirect
 from Aplicación1.form import *
 from Aplicación1.models import *
 
+import calendar
+from calendar import HTMLCalendar
+
 
 def Aplicación1(request):
     return render(request, "home.html")
@@ -47,3 +50,59 @@ def agregarPlanes(request):
 def archivoReportes(request):
     datos = {'archivoReportes': Reporte.objects.all()}
     return render(request, 'reportes.html', datos)
+
+def modificarClientes(request, id_cliente):
+    cliente = Cliente.objects.get(id_cliente=id_cliente)
+
+    if request.method == "POST":
+        formulario = ClienteForm(request.POST, instance=cliente)
+
+        if formulario.is_valid():
+            formulario.save()
+            return redirect('clientes')
+
+    else:
+        formulario = ClienteForm(instance=cliente)
+
+    datos = {'form': formulario}
+
+    return render(request, 'modificarClientes.html', datos)
+
+def eliminarClientes(request, id_cliente):
+    cliente = Cliente.objects.get(id_cliente=id_cliente)
+    cliente.delete()
+    return redirect('clientes')
+
+def modificarPlanes(request, id_plan):
+    planes = Plan_reciclaje.objects.get(id_plan=id_plan)
+
+    if request.method == "POST":
+        formulario = PlanForm(request.POST, instance=planes)
+
+        if formulario.is_valid():
+            formulario.save()
+            return redirect('planes')
+
+    else:
+        formulario = PlanForm(instance=planes)
+
+    datos = {'form': formulario}
+
+    return render(request, 'modificarPlanes.html', datos)
+
+def eliminarPlanes(request, id_plan):
+    planes = Cliente.objects.get(id_plan=id_plan)
+    planes.delete()
+    return redirect('planes')
+
+def calendario(request, year, month):
+    month = month.capitalize()
+    month_num = list(calendar.month_name).index(month)
+    month_num = int(month)
+
+    cal = HTMLCalendar().formatmonth(year, month_num)
+
+    datos = {"year": year, "month": month, "month_num": month_num, "cal": cal}
+
+    return render(request, 'calendario.html', datos)
+
